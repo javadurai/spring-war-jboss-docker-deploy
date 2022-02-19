c4084951682958556b7d4e10df8f1f061c6c2faf
Jabadurai Selvaraj <jabadurai.s@gmail.com>
2022-02-18 22:24:51 -0600

    Initial Commit

---
+++ spring-war-jboss-docker-deploy/src/main/java/com/test/MyController.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "This app is running on JBoss";
+	}
+
+}
